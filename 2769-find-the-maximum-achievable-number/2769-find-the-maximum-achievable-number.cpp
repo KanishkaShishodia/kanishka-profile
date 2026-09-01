5f149b81f50d2d0217c3a734a8df60e56dca72bc
Kanishka Shishodia <kanishka.25b01010456@abes.ac.in>
2026-09-01 15:39:02 +0530

    Time: 0 ms (100%), Space: 9.6 MB (78.59%) - LeetHub

--- 2769-find-the-maximum-achievable-number/2769-find-the-maximum-achievable-number.cpp
+++ 2769-find-the-maximum-achievable-number/2769-find-the-maximum-achievable-number.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
-    int theMaximumAchievableX(int num, int t) {
-        int k=num+2*t;
-        return k;
+    int theMaximumAchievableX(int num, int t){
+        return num+2*t;
     }
 };
