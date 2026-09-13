17f6c37c0fc798bc9eec9757eee0aecf18fc4401
Kanishka Shishodia <kanishka.25b01010456@abes.ac.in>
2026-09-13 20:43:05 +0530

    Time: 17 ms (5.34%), Space: 11.6 MB (89.31%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        int cnt=0;
+        for(int i=0;i<ransomNote.size();i++){
+            for(int j=0;j<magazine.size();j++){
+                if(ransomNote[i]==magazine[j]){
+                    cnt++;
+                    magazine.erase(magazine.begin()+j);
+                    break;
+                }
+            }
+        }
+        if(cnt==ransomNote.size())
+        return true;
+        else
+        return false;
+    }
+};
